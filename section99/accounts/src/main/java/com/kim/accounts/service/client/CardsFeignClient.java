b38aba85af451191e5f996948d6ad4146aee6cc8
Gideon <111112485+Gideonkim96@users.noreply.github.com>
2024-05-07 12:24:46 +0300

    Add files via upload

---
+++ section99/accounts/src/main/java/com/kim/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,18 @@
+package com.kim.accounts.service.client;
+
+
+import com.kim.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("cards")
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("kimbank-correlation-id")String correlationId,
+            @RequestParam String mobileNumber);
+
+}
---
+++ section99/accounts/src/main/java/com/kim/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,21 @@
+package com.kim.accounts.service.client;
+
+
+
+
+import com.kim.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("kimbank-correlation-id")String correlationId,
+            @RequestParam String mobileNumber);
+
+}
+
